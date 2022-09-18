b026b6dd836dc06e91a1129016a64e989a752221
Noah S-W <noahsw@users.noreply.github.com>
2022-09-17 20:28:17 -0700

    Start of pretty table

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import useGoogleSheets from 'use-google-sheets';
+import PlayerStandingModel from './PlayerStandingModel';
 import './App.css';
 
 const App = () => {
@@ -17,13 +18,39 @@ const App = () => {
     return <div>Error!</div>;
   }
 
-  return (
-    <div>
-      <div>{JSON.stringify(data)}</div>
+  const standings = data[0].data.map(row => row as PlayerStandingModel);
 
-      <button onClick={refetch}>Refetch</button>
-    </div>
+  return (
+    <>
+      <table>
+        <thead>
+          <tr>
+            <td>Player</td>
+            <td>Record</td>
+            <td>Point Diff</td>
+            <td>Week Result</td>
+          </tr>
+        </thead>
+        <tbody>
+          {standings.map(player => (
+            <StandingsRow key={player.Player} player={player} />
+          ))}
+        </tbody>
+      </table>
+
+      <br/>
+      <button onClick={refetch}>Refresh</button>
+    </>
   );
 };
+
+const StandingsRow = ({ player }: { player: PlayerStandingModel }) => (
+  <tr>
+    <td>{player.Player}</td>
+    <td>{player.Wins}-{player.Losses}-{player.Ties}</td>
+    <td>{player.PointDiff}</td>
+    <td>{player.WeekResult}</td>
+  </tr>
+);
 
 export default App;
---
+++ src/PlayerStandingModel.tsx
@@ -0,0 +1,13 @@
+export default interface PlayerStandingModel {
+  Player: string;
+  Wins: number;
+  Losses: number;
+  Ties: number;
+  PointDiff: number;
+  WeekResult: string;
+  WeekDiff: number;
+  Last10W: number;
+  Last10L: number;
+  Last10: string;
+  WeekTeams: string;
+}
